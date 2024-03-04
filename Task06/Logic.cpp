92c8ba1a95f26037e48f85837417c74ec551b3ba
Temashevk <a.shevko2008@gmail.com>
2024-03-04 17:30:23 +0300

    task06 done

---
+++ Task06/Logic.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+//task 06
+int find_abs_max_element_index(int* vector, int length) {
+	int max = abs(*vector);
+	int max_index = 0;
+
+	for (int i = 1; i < length; i++)
+	{
+		int t = abs(*(vector + i));
+
+		if (t > max) {
+			max = t;
+			max_index = i;
+		}
+	}
+
+	return max_index;
+}
+
+int find_first_positive_element_index(int* vector, int length) {
+	int index = 0;
+	for (int i = 0; i < length; i++)
+	{
+		if (*(vector + i) > 0) {
+			index = i;
+			break;
+		}
+	}
+
+	return index;
+}
+
+int find_sum_after_positive_element(int* vector, int length, int index) {
+	int sum = 0;
+
+	for (int i = index + 1; i < length; i++)
+	{
+		sum += *(vector + i);
+	}
+
+	return sum;
+}
---
+++ Task06/Main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+void random_init(int* vector, int size, int min, int max);
+string output(int* vector, int size);
+
+int find_abs_max_element_index(int* vector, int length);
+int find_first_positive_element_index(int* vector, int length);
+int find_sum_after_positive_element(int* vector, int length, int index);
+
+int main() {
+	int size = 10;
+	int* vector = new int[size];
+
+	random_init(vector, size, 0, 9);
+	int max_abs_index = find_abs_max_element_index(vector, size);
+	int first_positive = find_first_positive_element_index(vector, size);
+
+	cout << "Vector:\n" << output(vector, size) << endl;
+	cout << "Sum of all elements after first positive element:\n"
+		<< find_sum_after_positive_element(vector, size, first_positive) << endl;
+	cout << "Max abs element index: " << max_abs_index;
+
+	return 0;
+}
